de4acbd334043006134005ee92d720b9894dcf9e
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-11-18 21:23:45 +0800

    第七次提交
    定义一个指针a，a中给分配四个动态存储空间，（int
    ＊a）表示强制转换为int型。如果a＝0，则释放存储空间，＊a给它赋值为100，输出a的地址，＊a的值

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -10,15 +10,13 @@
 
     int main(int argc, const char * argv[]) {
         // insert code here...
-        int a[4];
-        int *p;
-        a[0]=10;
-        a[1]=20;
-        a[2]=30;
-        a[3]=40;
-        p=a;
-        printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x \n",&a[0],&a[1],&a[2],&a[3],a);
-        printf("a[0]=%d \n",*(p+2));
+        int *a;
+        a=(int *)malloc(4);
+        if(a==0)
+            exit(0);
+        *a=100;
+        printf("a=%x *a=%x\n",a,*a);
+        
         return 0;
     }
 
