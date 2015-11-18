3616531cf0b8ba18aea9a08cd411e254a793dc52
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-11-18 20:54:32 +0800

    第六次提交
    在第四次提交的基础上再输出，这次是通过指针p来访问，指针p中存放的是a的地址，通过间接取值得出a[2] 的值＊（p＋2）＝a［2］＝p［2］

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -5,11 +5,21 @@
 //  Created by 李晓晋 on 15/11/18.
 //  Copyright © 2015年 李晓晋. All rights reserved.
 //
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+
+    int main(int argc, const char * argv[]) {
+        // insert code here...
+        int a[4];
+        int *p;
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        a[3]=40;
+        p=a;
+        printf("&a[0]=%x &a[1]=%x &a[2]=%x &a[3]=%x a=%x \n",&a[0],&a[1],&a[2],&a[3],a);
+        printf("a[0]=%d \n",*(p+2));
+        return 0;
+    }
+
+
