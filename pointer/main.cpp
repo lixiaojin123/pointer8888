b26de790e06669ea9828c3e8b2228751e2aa039e
lixiaojin123 <20141105062l@fanydeimac13.local>
2015-11-18 18:07:45 +0800

    Initial Commit

---
+++ pointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by 李晓晋 on 15/11/18.
+//  Copyright © 2015年 李晓晋. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
