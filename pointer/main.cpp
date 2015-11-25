c768a985e7936afdb788729034a0d0d9b3be33dd
lixiaojin123 <20141105062l@FanydeiMac13.local>
2015-11-25 19:27:55 +0800

    第九次提交
    宏定义一个N为5，a中可以存储5个数组，定义整型变量i，j，temp，第一个for循环是输入5个数，第二个循环表示执行冒泡函数的次数，指的是执行
    外层的次数，都三个循环是执行冒泡函数内层的次数，就是每进行一次循环，把最大的值放到最后，if
    比较的是两个相邻的数的大小，如果前面钓大于后面，交换两个数，最后输出这几个数，从小到大

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -6,16 +6,27 @@
 //  Copyright © 2015年 李晓晋. All rights reserved.
 //
 #include <iostream>
-
-
-    int main(int argc, const char * argv[]) {
+#define N 5
+int main(int argc, const char * argv[]) {
         // insert code here...
-        int *a;
-        a=(int *)malloc(4);
-        if(a==0)
-            exit(0);
-        *a=100;
-        printf("a=%x    *a=%x\n",a,*a);
+        int a[N];
+        int i,j,temp;
+        for(i=0;i<N;i++)
+            scanf("%d",&a[i]);
+        for(i=0;i<N-1;i++)
+        {
+            for(j=0;j<N-1-i;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                 }
+            }
+        }
+        for (i=0;i<N;i++)
+           printf("%d\n",a[i]);
         return 0;
     }
 
